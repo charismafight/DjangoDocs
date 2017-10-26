8da0811fda011961833df79ea1dde53cb80654f9
lee <charismafight@hotmail.com>
2017-10-26 21:05:32 +0800

    http404 class

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 
 # Create your views here.
@@ -9,14 +10,16 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     # output = ','.join([x.question_text for x in latest_question_list])
     template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not found")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
