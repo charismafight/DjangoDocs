68ba90f4a768027eff070ea02de389bac042fb9b
Lee <charismafight@hotmail.com>
2017-10-27 16:59:49 +0800

    part of part 4

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -10,12 +13,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date > timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    mytest=models.CharField(max_length=10,default="")
+    mytest = models.CharField(max_length=10, default="")
 
     def __str__(self):
         return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -25,4 +25,5 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+
     return HttpResponse("you're voting on %s." % question_id)
