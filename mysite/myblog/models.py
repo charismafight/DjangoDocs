c542298b1d569dc3d97a3db0903b8ad3e0d8a967
Lee <charismafight@hotmail.com>
2017-11-04 17:21:21 +0800

    aggregation

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg
 
 
 # Create your models here.
@@ -40,4 +41,42 @@ class Entry(models.Model):
         return self.headline
 
 
-Entry
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+    num_awards = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    authors = models.ManyToManyField(Author)
+    publishers = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
+    registered_users = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    data = models.IntegerField()
+
+    class Meta:
+        ordering = ['name']
+
+
+Item
