b75cf239298c05e2c2fafe52b09c18840a0c44f1
lee <charismafight@hotmail.com>
2017-12-04 18:55:07 +0800

    cache

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -115,3 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CACHES={
+    'default':{
+        'BACKEND':'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION':'mysite_cache'
+    }
+}
